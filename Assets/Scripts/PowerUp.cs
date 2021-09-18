125fe50edf86c23da817647740b4b3b11542f29d
RustyPaper <adrianpodczaszy@gmail.com>
2021-09-18 17:24:08 +0200

    uploading full project

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.0f;
+    [SerializeField]
+    private int powerupID;
+    [SerializeField]
+    private AudioClip _clip;
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        
+
+        if (transform.position.y < -5f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Player player = other.transform.GetComponent<Player>();
+
+            AudioSource.PlayClipAtPoint(_clip, transform.position);
+
+            if (player!= null)
+            {
+                switch (powerupID)
+                {
+                    case 0:                      
+                        player.TripleShotActive();
+                        break;
+                    case 1:
+                        player.SpeedBoostActive();
+                        break;
+                    case 2:
+                        player.ShieldActive();
+                        break;
+                    default:
+                        Debug.Log("Default Value");
+                        break;
+                }
+            }
+            Destroy(this.gameObject);
+        }
+
+    }
+}
